058b70cb69c87c1bf224abbbc55c2b125f0add61
Julio <jtorres1989@gmail.com>
2018-12-19 13:25:45 -0500

    working filter function and set interval

--- src/App.js
+++ src/App.js
@@ -7,20 +7,21 @@ import Trains from './components/trains';
 // let subscription = 'fdf972f9606c4e7eb42faba59492f567'
 class App extends Component {
 
-
-  
-  
   constructor(props) {
     super(props)
     
     this.state = {
-      trainPositions: []
+      trainPositions: [],
+      filterByRed: false
     }
 
-    this.filterByColor.bind(this)
+    // this.filterByColor.bind(this)
+
+    setInterval(() => { this.handleFetch()
+    }, 1000);
+
   }
 
-  
   
   handleFetch = () => {
     let api_key = process.env.REACT_APP_API_KEY
@@ -31,24 +32,45 @@ class App extends Component {
   .then(results => this.setState({
     trainPositions: results.TrainPositions
   }))
+
 }
 
-filterByColor = (event) => {
 
-  console.log(this)
-  // this.state.trainPositions.filter((this.state.trainPositions.LineCode))
+updateFilterByColor = (event) => {
+    
+        this.setState({
+          filterByRed: true
+        })
+
 }
 
 
 
 render() {
-  // let dropDownList = document.getElementsByClassName("color")
-  // console.log("color", dropDownList)
-  // debugger
+
+let newTrain;
+
+  if (this.state.filterByRed === true) {
+   newTrain = this.state.trainPositions.filter((train) => {
+      // return console.log(train)
+      return train.LineCode === "RD"
+    })
+  } else {
+    newTrain = this.state.trainPositions
+  }
+    
+  
     return (
       <div className="App">
-        <Trains handleFetch={this.handleFetch} trains={this.state.trainPositions} 
+      <div>
+        <button name="RD" className="btn" onClick={this.updateFilterByColor}>Color Filter</button>
+        
+        
+      </div>
+        <Trains handleFetch={this.handleFetch} trains={newTrain} 
         filterByColor={this.filterByColor} />
+}
+        
       </div>
     );
   }
--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -54,12 +54,12 @@ class Dropdown extends React.Component {
           <div className="colorList">
           <ul >
             
-            <li key="RD" onClick={this.props.filterByColor.bind(this)} className="color">Red</li>
-            <li key="BL" onClick={this.props.filterByColor.bind(this)} className="color">Blue</li>
-            <li key="YL" onClick={this.props.filterByColor.bind(this)} className="color">Yellow</li>
-            <li key="OR" onClick={this.props.filterByColor.bind(this)} className="color">Orange</li>
-            <li key="GR" onClick={this.props.filterByColor.bind(this)} className="color">Green</li>
-            <li key="SV" onClick={this.props.filterByColor.bind(this)} className="color">Silver</li>
+            <li key="RD" onClick={this.props.filterByColor} className="color">Red</li>
+            <li key="BL" onClick={this.props.filterByColor} className="color">Blue</li>
+            <li key="YL" onClick={this.props.filterByColor} className="color">Yellow</li>
+            <li key="OR" onClick={this.props.filterByColor} className="color">Orange</li>
+            <li key="GR" onClick={this.props.filterByColor} className="color">Green</li>
+            <li key="SV" onClick={this.props.filterByColor} className="color">Silver</li>
           </ul>
           </div>
         
